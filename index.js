37efdcb4558b68158b48a5ad63df79e3eda5fbb0
Joel Blanco <jblnco.ing@example.com>
2021-02-19 15:37:17 -0400

    - Se cambia el texto con css3

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ for (let i = 1; i <= 70; i++) {
 const animateHearts=()=>{
     anime({
         targets:'.heart',
-        translateX: ()=>anime.random(-70,70) * 10,
+        translateX: ()=>anime.random(-70,70) * 15,
         translateY: ()=>anime.random(-50,50)*10,
         rotate: 45,
-        scale: ()=>anime.random(1,3),
+        scale: ()=>anime.random(1,6),
         easing: 'easeInOutBack',
         duration:3000,
         delay: anime.stagger(10),
